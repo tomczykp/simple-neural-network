bb45c8803c6e6055bd967b536e7a89a9daabedeb
tomczykp <tomczykp987@gmail.com>
2022-06-30 20:18:19 +0200

    fix gitignore

--- matrix.js
+++ matrix.js
@@ -9,7 +9,23 @@ const add = (m1, m2) => overElemOp(m1, m2, (x, y) => x + y);
 const sub = (m1, m2) => overElemOp(m1, m2, (x, y) => x - y);
 
 const mul = (m1, m2) => {
-  return m1.map((row, rowI) => {row.reduce((x, xI) => x * m2[xI][rowI])});
+  N = m1.length;
+  res = [];
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < N; j++) {
+      res[i][j] = 0;
+      for (let k = 0; k < N; k++)
+        res[i][j] += mat1[i][k] * mat2[k][j];
+    }
+  }
+
+  for (row of m1) {
+    for () {
+    }
+  }
+
+  return res;
+  //  return m1.map((row, rowI) => {row.reduce((x, xI) => x * m2[xI][rowI])});
 };
 
 module.export = {
